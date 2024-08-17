412b75c4c382195912094fd21d62b52e27733a61
Wilcorn55 <blakewilburn@hotmail.com>
2024-08-17 13:08:20 +1200

    First

---
+++ A Bards Scale/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizonalInput;
+    float moveSpeed = 10f;
+    bool isFacingRight = false;
+
+    public float jumpPower = 4f;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizonalInput = Input.GetAxis("Horizontal");
+        FlipSprite();
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizonalInput * moveSpeed, rb.velocity.y);
+    }
+
+    void FlipSprite() {
+
+        if (isFacingRight && horizonalInput < 0f || !isFacingRight && horizonalInput > 0f)
+        {
+
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
