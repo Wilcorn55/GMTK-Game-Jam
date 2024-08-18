7a312086c113681e57e3101cd482227a78ac7650
Wilcorn55 <blakewilburn@hotmail.com>
2024-08-18 15:40:27 +1200

    Moving Platforms
    Made the basis of the Moving Platforms

--- A Bards Scale/Assets/Scripts/MovingPlatform.cs
+++ A Bards Scale/Assets/Scripts/MovingPlatform.cs
@@ -10,11 +10,6 @@ public class MovingPlatform : MonoBehaviour
     int goalPoint = 0;
     public float moveSpeed = 2;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -25,11 +20,14 @@ public class MovingPlatform : MonoBehaviour
     void MoveToNextPoint() 
     {
         //move the platform towards the goal point
-        platform.position = Vector2.MoveTowards(platform.position, points[goalPoint].position, 1*Time.deltaTime*moveSpeed);
+        platform.position = Vector2.MoveTowards(platform.position, points[goalPoint].position, Time.deltaTime*moveSpeed);
 
         if(Vector2.Distance(platform.position, points[goalPoint].position) < 0.1f)
         {
-
+            if (goalPoint == points.Count - 1)
+                goalPoint = 0;
+            else
+                goalPoint++;
         }
     }
 }
